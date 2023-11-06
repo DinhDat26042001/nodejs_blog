fd82f65c318b454106b8715f474b4411cbea03ab
DinhDat26042001 <truongdinhdat26401.com>
2023-11-06 16:18:00 +0700

    create routes, create controller folder

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,14 @@
+
+class SiteController {
+
+    //GET /Site
+    index(req, res) {
+        res.render('home');
+    }
+
+    search(req, res) {
+        res.render('SEARCH')
+    }
+}
+
+module.exports = new SiteController;
